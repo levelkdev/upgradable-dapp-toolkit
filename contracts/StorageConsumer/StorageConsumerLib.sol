400a71b5ae8afc44610c56102d2ac71b0cdb145d
Mike Calvanese <mfcalvanese@gmail.com>
2018-02-18 16:07:15 -0500

    Refactor the storage consumer contracts
    they don't need to use a struct. Libraries can deal directly with BaseStorage instead of the struct.
    
    also `StorageStateful` felt like a better name

--- contracts/StorageConsumer/StorageConsumerLib.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.18;
-
-import "../Storage/BaseStorage.sol";
-
-library StorageConsumerLib {
-  struct Storage {
-    BaseStorage store;
-  }
-}
---
+++ contracts/StorageConsumer/StorageStateful.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.18;
+
+import "../Storage/BaseStorage.sol";
+
+contract StorageStateful {
+  BaseStorage _storage;
+}
